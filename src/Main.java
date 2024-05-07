75d365287dca6f3ea205308f2bf75c27a46d4f5a
Leysen95 <mullayanova95@bk.ru>
2024-05-07 21:13:35 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        byte apple = 83;
+        System.out.println("Яблок " + apple + " кг");
+        int oranges = 326;
+        System.out.println("Апельсинов " + oranges + " кг");
+        short watermelons = 175;
+        System.out.println("Арбузов " + watermelons + " кг");
+        long grapes = 1329;
+        System.out.println("Винограда " + grapes + " кг");
+        float tomatoes = 5.7F;
+        System.out.println("Помидор " + tomatoes + " кг");
+        double cucumbers = 3.87697857;
+        System.out.println("Огурцов " + cucumbers + " кг");
+    }
+}
