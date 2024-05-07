1aab1c1445660f3a3fde15cdf4d237faacfbab35
Leysen95 <mullayanova95@bk.ru>
2024-05-07 21:20:21 +0500

    task 2 done

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,20 @@ public class Main {
         System.out.println("Помидор " + tomatoes + " кг");
         double cucumbers = 3.87697857;
         System.out.println("Огурцов " + cucumbers + " кг");
+        float a = (float) 27.12;
+        System.out.println(a);
+        long b = 987678965549L;
+        System.out.println(b);
+        double c = 2.786;
+        System.out.println(c);
+        int d = 569;
+        System.out.println(d);
+        short e = -159;
+        System.out.println(e);
+        int f = 27897;
+        System.out.println(f);
+        byte g = 67;
+        System.out.println(g);
+
     }
 }
